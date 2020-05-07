553c0d343d6994339f361797b5469c3ab5b91a99
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2020-05-07 02:06:03 -0600

    Implemented solution

--- security_system.py
+++ security_system.py
@@ -4,6 +4,29 @@
 # Define any variables or functions you need.
 # The tests are testing your program's output.
 
+users = {
+    'mhenderson': 'out',
+    'jjabrams': 'out',
+    'sbrown': 'out',
+    'enterocc': 'out',
+    'zzdawg': 'out'
+}
+
 if __name__ == '__main__':
-    # Write your code here
-    print("not implemented")
+    err_msg = '  ALARM SOUNDED'
+    while True:
+        user_input = input('Input: ')
+        input_arr = user_input.split()
+        if len(input_arr) != 2:
+            print(err_msg)
+            continue
+
+        username = input_arr[0]
+        action = input_arr[1]
+
+        if username not in users or action != 'in' and action != 'out' or action == users[username]:
+            print(err_msg)
+        else:
+            users[username] = action
+            print(f'  {username} is checked {action}')
+
